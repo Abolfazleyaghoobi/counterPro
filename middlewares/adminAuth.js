ba316635d2eb2732639c49b068e70a5431d7cf65
Abolfazl Yaghoobi <80malard57@gmail.com>
2026-08-27 02:00:09 +0330

    Replace JWT secret with hardcoded token in adminAuth middleware

--- middlewares/adminAuth.js
+++ middlewares/adminAuth.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const adminAuth = (req, res, next) => {
   try {
     const token = req.cookies.adminToken;
-
+tok="de0e72aa3391c96fedbb1b909d0dcdc92d041ec320c8c45ae73e7ad171513d16fc440d763c8fd80149e4289c8bacb1e75ad69cb29e136293fe5f94a5ba6d71ac"
     if (!token) {
       return res.status(401).json({
         success: false,
@@ -13,7 +13,7 @@ const adminAuth = (req, res, next) => {
 
     const decoded = jwt.verify(
       token,
-      process.env.JWT_SECRET
+    tok
     );
 
     if (decoded.role !== "admin") {
